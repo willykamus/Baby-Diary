fd3c7532522582420fda5806f90eb37e9acb48bc
William Ching <45368980+willykamus@users.noreply.github.com>
2023-12-17 20:26:42 -0500

    Add files via upload

---
+++ Baby Diary/Baby_DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Baby_DiaryApp.swift
+//  Baby Diary
+//
+//  Created by William Ching on 2023-12-17.
+//
+
+import SwiftUI
+
+@main
+struct Baby_DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
